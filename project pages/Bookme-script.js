f575ca189cc90b0c77044d212c0f99f7f5f9fb13
RaminKurkeice <55253781+RaminKurkeice@users.noreply.github.com>
2020-01-29 18:34:04 -0500

    added directory
    added directory for the webpages add your webpages below with a discription of what you did so i can add it to the main page. thanks

---
+++ project pages/Bookme-script.js
@@ -0,0 +1,35 @@
+var i = 0; // start point
+var images = [];
+var time = 3000;
+
+
+// image list
+images[0] = 'CollegeBooks.jpg';
+images[1] = 'School discussion.jpg';
+images[2] = 'students working.jpg';
+images[3] = 'textbooks.jpg';
+
+// change images
+function changeImg(){
+document.slide.src = images[i];
+
+if(i < images.length - 1){
+	
+	i++; 
+
+}else{
+	
+	i = 0;
+
+}
+
+setTimeout("changeImg()", time);
+
+}
+
+function search(){
+
+
+}
+
+window.onload = changeImg;
