bc0bac41301e97339d0de4fbb9a2c745b3e3f2fd
RaminKurkeice <55253781+RaminKurkeice@users.noreply.github.com>
2020-02-21 09:16:12 -0500

    Bookme v3
    finished most of the pages login has some bugs but mostly working

--- project pages/login.js
+++ project pages/login.js
@@ -1,56 +1,49 @@
 $(document).ready(init);
 
 function init(){
-	$(".error").hide();
-	$("#submit").click(validate);
+	$('.error').hide();
+	$('#submit').click(validate);
+	
 }
 
 
 function validate(event){
-	$(".error").hide();
-	var hasNumber = /\d/;
 	
-	var fname = $("input#firstname").val();
-	if (fname == ""){
-		$("label#fname_error").show();
-		$("input#firstname").focus();
-		return false;
-	}
-	else if (hasNumber.test("fname")){
-		$("label#fname_error").show();
-		$("input#firstname").focus();
-		return false;
-	}
-		var lname = $("input#lastname").val();
-	if (lname == ""){
-		$("label#lname_error").show();
-		$("input#lastname").focus();
-		return false;
-	}
-	else if (hasNumber.test("lname")){
-		$("label#lname_error").show();
-		$("input#lastname").focus();
-		return false;
-	}
+	$(".error").hide();
+	
+	var password = $("input#password").val();
 	var email = $("input#email").val();
-	if (email == ""){ 
+	var fname = $("input#firstname").val();
+	var lname = $("input#lastname").val();
+	
+	
+	if (fname == "" || fname == /[0-9]/.test("fname")){
+		$("label#fname_error").show();
+		$("input#firstname").focus();
+		return false;
+	}
+	
+	else if (lname == "" || lname == /[0-9]/.test("lname")){
+		$("label#lname_error").show();
+		$("input#lastname").focus();
+		return false;
+	}
+	
+	else if (email == ""){ 
 		$("label#email_error").show();
 		$("input#email").focus();
 		return false;
 	}
-	var password = $("input#password").val();
-	if (password == ""){
+	else if (password == "" || password.length <= 8 || password != /[a-z]/.test(password) || password == /[^\w\s]/.test(password) || password != /[0-9]/.test(password)){
 		$("label#password_error").show();
 		$("input#password").focus();
 		return false;
 	}
-	else if(password.length <= 8 && /[a-z]/.test(password) || /[A-Z]/.test(password) && /\d/.test(password) && /[^\w\s]/.test(password)){
-		$("label#password_error").show();
-		$("input#password").focus();
-		return false;
+	else {
+		window.alert("confirmed redirecting ...");
+		window.open("Bookme-MainPage.html");
+	return true;
 	}
-	  var dataString = 'name='+ fname + lname +'&email=' + email + '&password=' + phone;
-  alert (dataString);
-  return false;
-
+	
+	return false;
 }
